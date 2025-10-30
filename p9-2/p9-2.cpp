a7c779cf87dc0fc190156594b963d68733145ba8
Sungrae Hong <s-hong.1@onu.edu>
2025-10-30 12:33:26 -0400

    Completed isSortedDecreasing

--- p9-2/p9-2.cpp
+++ p9-2/p9-2.cpp
@@ -3,19 +3,32 @@
 using namespace std;
 
 bool isSortedIncreasing(int arr[], int size);
+bool isSortedDecreasing(int arr[], int size);
 
 int main() {
 	int array[6] = { 1, 3, 5, 7, 9, 11 };
 
 	if (isSortedIncreasing(array, 6)) cout << "The data are increasing.\n";
 	else cout << "The data are not increasing.\n";
+	
+	if (isSortedDecreasing(array, 6)) cout << "The data are decreasing.\n";
+	else cout << "The data are not decreasing.\n";
 
 	return 0;
 }
 
 bool isSortedIncreasing(int arr[], int size) {
 	for (int i = 1; i< size; i++) {
-		if (arr[i] < arr[i - 1]) {
+		if (arr[i] <= arr[i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool isSortedDecreasing(int arr[], int size) {
+	for (int i = 1; i < size; i++) {
+		if (arr[i] >= arr[i - 1]) {
 			return false;
 		}
 	}

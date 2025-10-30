d9a4cd0a7f74fb10c1c89d7b2a2fd9456bc59645
Sungrae Hong <s-hong.1@onu.edu>
2025-10-30 12:38:34 -0400

    Completed hasDuplicates

--- p9-2/p9-2.cpp
+++ p9-2/p9-2.cpp
@@ -5,6 +5,7 @@ using namespace std;
 bool isSortedIncreasing(int arr[], int size);
 bool isSortedDecreasing(int arr[], int size);
 bool hasAdjecentDuplicates(int arr[], int size);
+bool hasDuplicates(int arr[], int size);
 
 int main() {
 	int array[6] = { 1, 3, 3, 7, 9, 11 };
@@ -17,6 +18,9 @@ int main() {
 
 	if (hasAdjecentDuplicates(array, 6)) cout << "The data has adjacent duplicates.\n";
 	else cout << "The data has no adjacent duplicates.\n";
+
+	if (hasDuplicates(array, 6)) cout << "The data has duplicates.\n";
+	else cout << "The data has no duplicates.\n";
 
 	return 0;
 }
@@ -43,6 +47,17 @@ bool hasAdjecentDuplicates(int arr[], int size) {
 	for (int i = 1; i < size; i++) {
 		if (arr[i] == arr[i - 1]) {
 			return true;
+		}
+	}
+	return false;
+}
+
+bool hasDuplicates(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = i + 1; j < size; j++) {
+			if (arr[i] == arr[j]) {
+				return true;
+			}
 		}
 	}
 	return false;

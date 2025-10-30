a9a63ba875b0b4eed9feadcd06b8767729e786e2
Sungrae Hong <s-hong.1@onu.edu>
2025-10-30 12:35:33 -0400

    Completed hasAdjecentDuplicates

--- p9-2/p9-2.cpp
+++ p9-2/p9-2.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 bool isSortedIncreasing(int arr[], int size);
 bool isSortedDecreasing(int arr[], int size);
+bool hasAdjecentDuplicates(int arr[], int size);
 
 int main() {
-	int array[6] = { 1, 3, 5, 7, 9, 11 };
+	int array[6] = { 1, 3, 3, 7, 9, 11 };
 
 	if (isSortedIncreasing(array, 6)) cout << "The data are increasing.\n";
 	else cout << "The data are not increasing.\n";
@@ -14,12 +15,15 @@ int main() {
 	if (isSortedDecreasing(array, 6)) cout << "The data are decreasing.\n";
 	else cout << "The data are not decreasing.\n";
 
+	if (hasAdjecentDuplicates(array, 6)) cout << "The data has adjacent duplicates.\n";
+	else cout << "The data has no adjacent duplicates.\n";
+
 	return 0;
 }
 
 bool isSortedIncreasing(int arr[], int size) {
 	for (int i = 1; i< size; i++) {
-		if (arr[i] <= arr[i - 1]) {
+		if (arr[i] < arr[i - 1]) {
 			return false;
 		}
 	}
@@ -28,9 +32,18 @@ bool isSortedIncreasing(int arr[], int size) {
 
 bool isSortedDecreasing(int arr[], int size) {
 	for (int i = 1; i < size; i++) {
-		if (arr[i] >= arr[i - 1]) {
+		if (arr[i] > arr[i - 1]) {
 			return false;
 		}
 	}
 	return true;
+}
+
+bool hasAdjecentDuplicates(int arr[], int size) {
+	for (int i = 1; i < size; i++) {
+		if (arr[i] == arr[i - 1]) {
+			return true;
+		}
+	}
+	return false;
 }
